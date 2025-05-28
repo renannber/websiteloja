8d4aac0f7e130cd987448ff0ffd3cec75b5f14ad
renan berr <renannber.ads@gmail.com>
2025-05-27 21:57:29 -0300

    Criação das funções pesquisa cep e implementando uma condiçaão de verificação do cep

--- main.js
+++ main.js
@@ -59,3 +59,33 @@ function cadastrar() {
 const eNumero = (numero) => /^[0-9]+$/.teste(numero);    //expressão para ver se está entre 0 á 9
 
 const cepValido = (cep) => cep.length == 8 && eNumero(cep); // testa para ver se so tem numero e se é de 0 á 9
+
+const pesquisarCep = async() => {
+    limparFormulario();
+    const url = `viacep.com.br/ws/${cep.value}/json/`; //o ${cep.value} é usado para trocar o cep para o que for usado no cadastro
+      if(cepValido(cep.value))
+    {
+        const dados = await fetch(url); // await é uma pausa para verificar se o fetch vai conseguir da um retorno 
+
+        const addres = await dados.json();
+        //hasownproperty retorna um valor booleano indicando se o objeto possui a propriedae epecifica nos parentes 
+
+        if(addres.hasOwnProperty('erro')){
+         alert("CEP não encontrado");
+        } else{
+            preencherFormulario(addres);
+        }
+    }
+}
+
+preencherFormulario = (endereco) => {
+
+}
+//função para limpar o furmalrio 
+limparFormulario = () => {
+    document.getElementById('rua').value ="";
+    document.getElementById('bairro').value ="";
+    document.getElementById('cidade').value ="";
+    document.getElementById('estado').value ="";
+
+}
